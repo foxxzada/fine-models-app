f7cc8b1afa46ad884542848ae80824c5d325ae94
Gustavo Hariel <gustavobonja@gmail.com>
2024-08-05 19:56:52 -0300

    fix: next-themes provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,34 @@
 import type { Metadata } from "next";
-import "../styles/globals.css";
+import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Inter as FontSans } from "next/font/google";
 import { Providers } from "@/lib/providers/providers";
-import { ThemeProvider } from "@/lib/providers/theme-provider";
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+	subsets: ["latin"],
+	variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: "Chuck Facts",
-  description: "Find out more about Chuck Norris Facts!",
+	title: "Chuck Facts",
+	description: "Find out more about Chuck Norris Facts!",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable,
+				)}
+			>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
